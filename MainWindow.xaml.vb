a728c896b460a6600e48ff5ca210f39c0e685e47
crowsCaledfwlch <cnovosa@gmail.com>
2023-02-13 21:13:15 -0500

    Weekly assignment 3
    Made a list of events, changed the colours of some thing in the window and programmed the events into the buttons.

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,3 +1,22 @@
 ﻿Class MainWindow
+    Private Sub btnVeggie_Click(sender As Object, e As RoutedEventArgs) Handles btnVeggie.Click
+        picVeggie.Visibility = Visibility.Visible
+        picPrime.Visibility = Visibility.Hidden
+    End Sub
 
+    Private Sub btnPrime_Click(sender As Object, e As RoutedEventArgs) Handles btnPrime.Click
+        picPrime.Visibility = Visibility.Visible
+        picVeggie.Visibility = Visibility.Hidden
+
+    End Sub
+
+    Private Sub btnSelect_Click(sender As Object, e As RoutedEventArgs) Handles btnSelectMeal.Click
+        btnPrime.IsEnabled = False
+        btnVeggie.IsEnabled = False
+        btnExit.IsEnabled = True
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As RoutedEventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
 End Class
